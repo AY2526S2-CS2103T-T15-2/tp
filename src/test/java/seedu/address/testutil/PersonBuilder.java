d494d47eee38589c453b53990627076e8cb169c9
Clarence Lee <clarencelee88@gmail.com>
2026-03-05 11:41:35 +0800

    Fix test checkstyle issues

--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -30,6 +30,9 @@ public class PersonBuilder {
     private Remark remark;
     private Set<Tag> tags;
 
+    /**
+     * Creates a {@code PersonBuilder} with the default details.
+     */
     public PersonBuilder() {
         name = new Name(DEFAULT_NAME);
         phone = new Phone(DEFAULT_PHONE);
@@ -39,6 +42,9 @@ public class PersonBuilder {
         tags = new HashSet<>();
     }
 
+    /**
+     * Initializes the PersonBuilder with the data of {@code personToCopy}.
+     */
     public PersonBuilder(Person personToCopy) {
         name = personToCopy.getName();
         phone = personToCopy.getPhone();
@@ -48,36 +54,57 @@ public class PersonBuilder {
         tags = new HashSet<>(personToCopy.getTags());
     }
 
+    /**
+     * Sets the {@code Name} of the {@code Person} that we are building.
+     */
     public PersonBuilder withName(String name) {
         this.name = new Name(name);
         return this;
     }
 
+    /**
+     * Parses the {@code tags} into a {@code Set<Tag>} and sets it to the {@code Person}.
+     */
     public PersonBuilder withTags(String... tags) {
         this.tags = SampleDataUtil.getTagSet(tags);
         return this;
     }
 
+    /**
+     * Sets the {@code Address} of the {@code Person} that we are building.
+     */
     public PersonBuilder withAddress(String address) {
         this.address = new Address(address);
         return this;
     }
 
+    /**
+     * Sets the {@code Phone} of the {@code Person} that we are building.
+     */
     public PersonBuilder withPhone(String phone) {
         this.phone = new Phone(phone);
         return this;
     }
 
+    /**
+     * Sets the {@code Email} of the {@code Person} that we are building.
+     */
     public PersonBuilder withEmail(String email) {
         this.email = new Email(email);
         return this;
     }
 
+    /**
+     * Sets the {@code Remark} of the {@code Person} that we are building.
+     */
     public PersonBuilder withRemark(String remark) {
         this.remark = new Remark(remark);
         return this;
     }
 
+    /**
+     * Builds and returns a {@code Person}.
+     */
     public Person build() {
         return new Person(name, phone, email, address, remark, tags);
     }
